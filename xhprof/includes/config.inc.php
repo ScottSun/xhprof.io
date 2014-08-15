a344ba72012b5adf80fd6d6c12e92e39fcd54584
Jipeng Sun <jipeng.sun@funplus.com>
2014-08-15 15:39:00 +0800

    add uri prof

--- xhprof/includes/config.inc.php
+++ xhprof/includes/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'url_base' => 'http://dw-integration.socialgamenet.com/',
+	'url_base' => 'http://dw-integration.socialgamenet.com/prof/',
 	'url_static' => null, // When undefined, it defaults to $config['url_base'] . 'public/'. This should be absolute URL.
 	'pdo' => new PDO('mysql:dbname=xhprof;host=127.0.0.1;charset=utf8', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode = ""')),
 );
