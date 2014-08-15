e639a4b66e6c7d18196f38cc6c966be8be49b7c5
Jipeng Sun <jipeng.sun@funplus.com>
2014-08-15 15:02:58 +0800

    move xhprof to dev-integration

--- inc/append.php
+++ inc/append.php
@@ -5,33 +5,36 @@ if(php_sapi_name() == 'cli')
 	return;
 }
 
-register_shutdown_function(function(){
-	// by registering register_shutdown_function at the end of the file
-	// I make sure that all execution data, including that of the earlier
-	// registered register_shutdown_function, is collected.
+if (!isset($post_params['prof']) || $post_params['prof']) {
+    register_shutdown_function(function(){
+        // by registering register_shutdown_function at the end of the file
+        // I make sure that all execution data, including that of the earlier
+        // registered register_shutdown_function, is collected.
 
-	$xhprof_data	= xhprof_disable();
+        $xhprof_data	= xhprof_disable();
 
-	if(function_exists('fastcgi_finish_request'))
-	{
-		fastcgi_finish_request();
-	}
+        if(function_exists('fastcgi_finish_request'))
+        {
+            fastcgi_finish_request();
+        }
 
-    if ($_SERVER['HTTP_HOST'] == 'dw-dev.socialgamenet.com') {
-        // xhprof
-        $XHPROF_ROOT = '/sgn/htdocs/xhprof/';
-        include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
-        include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
-        $xhprof_runs = new XHProfRuns_Default();
-        global $run_id;
-        $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_dw_dev");
+        if ($_SERVER['HTTP_HOST'] == 'dw-dev.socialgamenet.com'
+            && (substr($_SERVER['REQUEST_URI'], 1, 4) == 'api')) {
+            // xhprof
+            $XHPROF_ROOT = '/sgn/htdocs/dev-dw/dragon-server-code/docroot/xhprof/';
+            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+            $xhprof_runs = new XHProfRuns_Default();
+            global $run_id;
+            $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_dw_dev");
 
-        // xhprof.io
-        $config			= require __DIR__ . '/../xhprof/includes/config.inc.php';
-        require_once __DIR__ . '/../xhprof/classes/data.php';
-        $xhprof_data_obj	= new \ay\xhprof\Data($config['pdo']);
-        $xhprof_data_obj->save($xhprof_data);
+            // xhprof.io
+            $config			= require __DIR__ . '/../xhprof/includes/config.inc.php';
+            require_once __DIR__ . '/../xhprof/classes/data.php';
+            $xhprof_data_obj	= new \ay\xhprof\Data($config['pdo']);
+            $xhprof_data_obj->save($xhprof_data);
 
-    }
-	
-});
+        }
+        
+    });
+}
--- inc/prepend.php
+++ inc/prepend.php
@@ -5,4 +5,8 @@ if(php_sapi_name() == 'cli')
 	return;
 }
 
-xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
+$post_params = json_decode(file_get_contents('php://input'));
+if (!isset($post_params['prof']) || $post_params['prof']) {
+    xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
+}
+
